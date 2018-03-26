e44c8dcd0e5fd88c9f5c455565a958265a6927c1
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-03-26 17:55:55 +0300

    fix: getting mongo uri from env variable

--- server/getdbconnection.js
+++ server/getdbconnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const logger = require('./logger');
-
+console.log('process.env.MONGO_URI', process.env.MONGO_URI);
 const connection = mongoose.createConnection('mongodb://nmikhaliuk:fd3218697@ds012168.mlab.com:12168/todo-app');
 connection.on('error', (error) => {
   console.log('connection error:');
