c94957a4fbeed13af25a08b952e26a131d90a8d1
Nikita Mikhaliuk <nikkita1992@mail.ru>
2024-01-01 21:58:33 +0300

    fix: missing static server config

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,6 +3,9 @@ import app from './index.js';
 
 const PORT = process.env.PORT ? +process.env.PORT : 3000;
 
+ViteExpress.config({
+    ignorePaths: (path) => path.includes('/api'),
+});
 ViteExpress.listen(app, PORT, () => {
     console.log(process.env.NODE_ENV);
     console.log(`Server is running on http://localhost:${PORT}...`);
