33d177e4274dc3959ec8b7838da7ae4e433eecb4
Nikita Mikhaliuk <nikkita1992@mail.ru>
2024-01-01 19:15:50 +0300

    hotfix: replace redux with useReduser & useEffect to prevent connect rendering issues

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -9,18 +9,111 @@ import MyTheme from './Theme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Route, withRouter } from 'react-router-dom';
-import actions from './actions';
+import actionCreators from './actions';
+import todoListReducer from './reducers/todoList';
+import appViewReducer from './reducers/appView';
+import { useEffect, useReducer } from 'react';
+import { fromJS } from 'immutable';
 
-function AppComponent(appProps: any) {
+function reducer(state, action) {
+    if (action.type === 'INITIAL_UPLOAD') {
+        return { ...state, ...action.payload };
+    }
+    return todoListReducer(appViewReducer(state, action), action);
+}
+
+interface State {
+    todoList: { toJS: () => object };
+    appView: {
+        toJS: () => Omit<State['appView'], 'toJS'>;
+        showCompleted: boolean;
+        filter: string;
+        chosenCategoryId: string;
+        chosenItemToEditId: string;
+    };
+}
+
+function mapState(state: State) {
+    return {
+        todoList: state.todoList ? state.todoList.toJS() : null,
+        showCompleted: state.appView.toJS().showCompleted,
+        filter: state.appView.toJS().filter,
+        chosenCategoryId: state.appView.toJS().chosenCategoryId,
+        chosenItemToEditId: state.appView.toJS().chosenItemToEditId,
+    };
+}
+
+function AppComponent({ location }: any) {
+    const [state, dispatch] = useReducer(reducer, {
+        appView: fromJS({
+            chosenCategoryId: null,
+            chosenItemToEditId: null,
+            filter: '',
+            showCompleted: false,
+        }),
+        todoList: fromJS(null),
+    });
+
+    useEffect(() => {
+        const username = window.location.pathname.split('/')[2];
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `/api/users/${username}/get-todolist`, true);
+        xhr.send();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) {
+                return;
+            }
+            if (this.status !== 200) {
+                return;
+            }
+            const todoList = JSON.parse(this.responseText);
+
+            const normalizedCatStorage = {};
+            const normalizedItemsStorage = {};
+            for (const item of todoList.itemsStorage) {
+                normalizedItemsStorage[item.id] = item;
+            }
+            for (const cat of todoList.categoriesStorage) {
+                normalizedCatStorage[cat.id] = cat;
+            }
+            const normalizedTodoList = {
+                ...todoList,
+                categoriesStorage: normalizedCatStorage,
+                itemsStorage: normalizedItemsStorage,
+            };
+            const uploadedState = {
+                todoList: fromJS(normalizedTodoList),
+                appView: fromJS({
+                    chosenCategoryId: null,
+                    chosenItemToEditId: null,
+                    showCompleted: false,
+                    filter: '',
+                }),
+            };
+            dispatch({
+                type: 'INITIAL_UPLOAD',
+                payload: uploadedState,
+            });
+        };
+    }, []);
+
     const {
         chosenCategoryId,
         chosenItemToEditId,
-        todoList,
-        actions,
         filter,
         showCompleted,
-        location,
-    } = appProps;
+        todoList,
+    } = mapState(state);
+
+    const actions = {};
+    for (const [actionName, action] of Object.entries(actionCreators)) {
+        actions[actionName] = (...args) => dispatch(action(...args));
+    }
+    if (!todoList) {
+        return <div>Loading</div>;
+    }
+
+    console.log(todoList);
     const chosenCategory = todoList.categoriesStorage[chosenCategoryId];
     return (
         <MuiThemeProvider muiTheme={getMuiTheme(MyTheme)}>
@@ -78,34 +171,15 @@ function AppComponent(appProps: any) {
     );
 }
 
-interface State {
-    todoList: { toJS: () => object };
-    appView: {
-        toJS: () => Omit<State['appView'], 'toJS'>;
-        showCompleted: boolean;
-        filter: string;
-        chosenCategoryId: string;
-        chosenItemToEditId: string;
-    };
-}
-
-function mapStateToProps(state: State) {
-    return {
-        todoList: state.todoList.toJS(),
-        showCompleted: state.appView.toJS().showCompleted,
-        filter: state.appView.toJS().filter,
-        chosenCategoryId: state.appView.toJS().chosenCategoryId,
-        chosenItemToEditId: state.appView.toJS().chosenItemToEditId,
-    };
-}
-
-function mapDispatchToProps(dispatch: unknown) {
-    return {
-        actions: bindActionCreators(actions, dispatch),
-    };
-}
+// function mapDispatchToProps(dispatch: unknown) {
+//     const boundActions = bindActionCreators(actionCreators, dispatch);
+//     return {
+//         actions: boundActions,
+//     };
+// }
 
 const App = withRouter(
-    connect(mapStateToProps, mapDispatchToProps)(AppComponent)
+    AppComponent
+    // connect(mapStateToProps, mapDispatchToProps)(AppComponent)
 );
 export default App;
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -9,56 +9,14 @@ import { fromJS } from 'immutable';
 
 import App from './App';
 
-let initialState;
+//const store = configureStore(initialState);
 
-const render = (Component: React.FC, store: any) => {
-    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <Router>
-                    <Component />
-                </Router>
-            </Provider>
-        </React.StrictMode>
-    );
-};
-const username = window.location.pathname.split('/')[2];
-const xhr = new XMLHttpRequest();
-xhr.open('GET', `/api/users/${username}/get-todolist`, true);
-xhr.send();
-xhr.onreadystatechange = function () {
-    if (this.readyState !== 4) {
-        return;
-    }
-    if (this.status !== 200) {
-        return;
-    }
-    const todoList = JSON.parse(this.responseText);
-
-    const normalizedCatStorage = {};
-    const normalizedItemsStorage = {};
-    for (const item of todoList.itemsStorage) {
-        normalizedItemsStorage[item.id] = item;
-    }
-    for (const cat of todoList.categoriesStorage) {
-        normalizedCatStorage[cat.id] = cat;
-    }
-    const normalizedTodoList = {
-        ...todoList,
-        categoriesStorage: normalizedCatStorage,
-        itemsStorage: normalizedItemsStorage,
-    };
-    initialState = {
-        todoList: fromJS(normalizedTodoList),
-        appView: fromJS({
-            chosenCategoryId: null,
-            chosenItemToEditId: null,
-            showCompleted: false,
-            filter: '',
-        }),
-    };
-    const store = configureStore(initialState);
-    render(App, store);
-};
-
-// render(App);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <Provider store={{}}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    </React.StrictMode>
+);
