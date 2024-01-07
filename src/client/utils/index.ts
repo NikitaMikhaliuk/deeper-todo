d5e5a9677f80f200837ca3e62a884d608f2c8298
Nikita Mikhaliuk <nikkita1992@mail.ru>
2024-01-07 18:25:01 +0300

    add: groupBy util

--- src/client/utils/index.ts
+++ src/client/utils/index.ts
@@ -5,3 +5,17 @@ export function urlToName(name: string) {
 export function nameToUrl(name: string) {
     return '/' + name.split(' ').join('-');
 }
+
+export function groupBy<T>(
+    items: Array<T>,
+    callbackFn: (value: T, index: number) => string | symbol
+) {
+    return items.reduce((groupedItems, item, index) => {
+        const groupKey = callbackFn(item, index);
+        if (!groupedItems[groupKey]) {
+            groupedItems[groupKey] = [];
+        }
+        groupedItems[groupKey].push(item);
+        return groupedItems;
+    }, {} as Record<string | symbol, T[]>);
+}
