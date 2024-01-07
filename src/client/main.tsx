6d3e984ca0fa56efddf1bcaa07a7883159b791c3
Nikita Mikhaliuk <nikkita1992@mail.ru>
2024-01-07 18:25:40 +0300

    change: adjusted client main imports

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,19 +1,18 @@
-import './index.css';
-
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
 
 import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
+    <StrictMode>
         <Provider store={store}>
             <Router>
                 <App />
             </Router>
         </Provider>
-    </React.StrictMode>
+    </StrictMode>
 );
