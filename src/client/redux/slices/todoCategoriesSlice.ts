8f3b79dd9b27e37739f1775853a1b849885e8ce5
Nikita Mikhaliuk <nikkita1992@mail.ru>
2024-01-09 03:55:23 +0300

    add: todoCategoriesSlice logic

--- src/client/redux/slices/todoCategoriesSlice.ts
+++ src/client/redux/slices/todoCategoriesSlice.ts
@@ -1,8 +1,94 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {
+    createSlice,
+    createEntityAdapter,
+    PayloadAction,
+    createSelector,
+} from '@reduxjs/toolkit';
+import { groupBy } from '../../utils';
+
+export type TodoCategory = {
+    id: string;
+    _id?: string;
+    parentCategoryId: string;
+    name: string;
+    completed: boolean;
+    visible?: boolean;
+};
+
+type TodoCategoryRenameOptions = {
+    id: string;
+    changes: Pick<TodoCategory, 'name'>;
+};
+
+const todoCategoriesAdapter = createEntityAdapter<TodoCategory>({});
+
+type InitialState = {
+    idsGroupedByParent: Record<string, string[]>;
+};
+
+type TodoCategoriesSliceState = InitialState &
+    ReturnType<typeof todoCategoriesAdapter.getInitialState>;
 
 export const todoCategoriesSlice = createSlice({
     name: 'todoCategories',
-    initialState: {},
+    reducerPath: 'todoCategories',
+    initialState: todoCategoriesAdapter.getInitialState<InitialState>({
+        idsGroupedByParent: {},
+    }),
+    reducers: {
+        setCategories: (state, action: PayloadAction<TodoCategory[]>) => {
+            todoCategoriesAdapter.setAll(state, action);
+            state.idsGroupedByParent = groupBy(
+                state.ids,
+                (id) => state.entities[id].parentCategoryId
+            );
+        },
+        addCategory: (state, action: PayloadAction<TodoCategory>) => {
+            todoCategoriesAdapter.addOne(state, action);
+            const { id, parentCategoryId } = action.payload;
+            const { [parentCategoryId]: parentCatChidIds } =
+                state.idsGroupedByParent;
+            parentCatChidIds.unshift(id);
+        },
+        renameCategory: (
+            state,
+            action: PayloadAction<TodoCategoryRenameOptions>
+        ) => {
+            todoCategoriesAdapter.updateOne(state, action.payload);
+        },
+        removeCategoryFromParent: (state, action: PayloadAction<string>) => {
+            const { id, parentCategoryId } = state.entities[action.payload];
+            let { [parentCategoryId]: parentCatChildIds } =
+                state.idsGroupedByParent;
+            parentCatChildIds = parentCatChildIds.filter(
+                (catId) => catId !== id
+            );
+            state.idsGroupedByParent[parentCategoryId] = parentCatChildIds;
+        },
+        deleteCategories: (state, action: PayloadAction<string[]>) => {
+            todoCategoriesAdapter.removeMany(state, action.payload);
+            for (const id of action.payload) {
+                delete state.idsGroupedByParent[id];
+            }
+        },
+    },
+    selectors: {
+        getCategoryById: todoCategoriesAdapter.getSelectors().selectById,
+    },
 });
+
+export const {
+    setCategories,
+    addCategory,
+    removeCategoryFromParent,
+    deleteCategories,
+} = todoCategoriesSlice.actions;
+export const { getCategoryById } = todoCategoriesSlice.selectors;
+
+export const makeGetcategoryIdsByParent = (parentId: string) =>
+    createSelector(
+        [(state: TodoCategoriesSliceState) => state],
+        (state) => state.idsGroupedByParent[parentId]
+    );
 
 export default todoCategoriesSlice.reducer;
