d2ecbf2a0815d51b5be425377f157abd0b6895f2
Nikita Mikhaliuk <nikkita1992@mail.ru>
2024-01-14 21:37:14 +0300

    fix: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,14 +10,11 @@ module.exports = {
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react', 'react-refresh'],
+    plugins: ['@typescript-eslint', 'react', 'react-refresh', 'react-hooks'],
     rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-        // 'no-debugger': 0,
-        // 'no-console': 1,
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
         'no-use-before-define': 2,
+        '@typescript-eslint/no-unused-vars': 0,
+        'no-unused-vars': ['warn', { ignorePatterns: ['*.ts*'] }],
     },
 };
