9fd65e4700cf5182addfe7e763a450f85a184d94
Nikita Mikhaliuk <nikkita1992@mail.ru>
2024-01-14 22:22:07 +0300

    fix: missing hmr updates for redux

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,18 +1,11 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import store from './redux/store';
 
-import App from './App';
+import AppWrapper from './AppWrapper';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
-        <Provider store={store}>
-            <Router>
-                <App />
-            </Router>
-        </Provider>
+        <AppWrapper />
     </StrictMode>
 );
